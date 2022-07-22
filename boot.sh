faf9e764f32cb33ce1bab2549b6b9f4665f5640e
Professor <ch-ctinf@ifsul.edu.br>
2022-07-22 00:53:23 -0300

    Add path to install script

--- boot.sh
+++ boot.sh
@@ -6,5 +6,5 @@
 
 sudo /home/professor/vm-config/restore.sh
 sudo /home/professor/vm-config/sync.sh
-sudo /home/professor/vm-config/install.sh > /home/professor/vm-config/log.log
+sudo PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' /home/professor/vm-config/install.sh
 sudo /home/professor/vm-config/block.sh
