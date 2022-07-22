82841d92678aad9e510059ba8d237989c3a0422f
Professor <ch-ctinf@ifsul.edu.br>
2022-07-22 16:56:42 -0300

    DO not block.

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@
 # all VMs.
 # -------------------------------------------------------------
 
+sudo add-apt-repository -y ppa:serge-rider/dbeaver-ce
+sudo apt-get install -y dbeaver-ce
+
+sudo -u aluno gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'chromium_chromium.desktop', 'firefox_firefox.desktop', 'code_code.desktop', 'sublime-text_subl.desktop', 'dbeaver-ce.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Calculator.desktop']"
