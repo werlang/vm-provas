12b9e5694e1fb4c99cd70a7c1691036e0bdddc62
Professor <ch-ctinf@ifsul.edu.br>
2022-07-22 16:25:36 -0300

    change aluno.zip, remove install commands

--- boot.sh
+++ boot.sh
@@ -6,5 +6,5 @@
 
 sudo /home/professor/vm-config/restore.sh
 sudo /home/professor/vm-config/sync.sh
-sudo PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' /home/professor/vm-config/install.sh
+# sudo PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' /home/professor/vm-config/install.sh
 sudo /home/professor/vm-config/block.sh
--- install.sh
+++ install.sh
@@ -5,9 +5,3 @@
 # all VMs.
 # -------------------------------------------------------------
 
-sudo mysql --user="root" --password="profs22inf" --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
-
-sudo add-apt-repository -y ppa:serge-rider/dbeaver-ce
-sudo apt-get install -y dbeaver-ce
-
-sudo -u aluno gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'chromium_chromium.desktop', 'firefox_firefox.desktop', 'code_code.desktop', 'sublime-text_subl.desktop', 'dbeaver-ce.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Calculator.desktop']"
