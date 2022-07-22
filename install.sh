77af529f1398301099d3777854ba49c31ade5771
Professor <ch-ctinf@ifsul.edu.br>
2022-07-22 01:28:33 -0300

    a

--- install.sh
+++ install.sh
@@ -11,3 +11,6 @@ sudo add-apt-repository -y ppa:serge-rider/dbeaver-ce
 sudo apt-get install -y dbeaver-ce
 
 sudo -u aluno gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'chromium_chromium.desktop', 'firefox_firefox.desktop', 'code_code.desktop', 'sublime-text_subl.desktop', 'dbeaver-ce.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Calculator.desktop']"
+
+sudo /home/professor/vm-config/restore.sh
+sudo dbeaver-ce
