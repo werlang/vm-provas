6cf0a69bc1dbf4bc909ad6db6766b0f5c2561be9
Professor <ch-ctinf@ifsul.edu.br>
2022-07-21 23:01:12 -0300

    add log to install.sh

--- boot.sh
+++ boot.sh
@@ -6,5 +6,5 @@
 
 sudo /home/professor/vm-config/restore.sh
 sudo /home/professor/vm-config/sync.sh
-sudo /home/professor/vm-config/install.sh
+sudo /home/professor/vm-config/install.sh > /home/professor/vm-config/log.log
 sudo /home/professor/vm-config/block.sh
