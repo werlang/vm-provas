6f3174ca90f23e93df7fe630a389bec0667922a9
Professor <ch-ctinf@ifsul.edu.br>
2022-07-21 22:02:29 -0300

    Update install script.

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@
 
 sudo mysql --user="root" --password="profs22inf" --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
 
-sudo add-apt-repository ppa:serge-rider/dbeaver-ce
-sudo apt-get install dbeaver-ce
+sudo add-apt-repository -y ppa:serge-rider/dbeaver-ce
+sudo apt-get install -y dbeaver-ce
