18b74ab033b914e807973af2d2071098b94a2f8d
Professor <ch-ctinf@ifsul.edu.br>
2022-07-21 22:07:02 -0300

    Update aluno favorites

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ sudo mysql --user="root" --password="profs22inf" --execute="ALTER USER 'root'@'l
 
 sudo add-apt-repository -y ppa:serge-rider/dbeaver-ce
 sudo apt-get install -y dbeaver-ce
+
+sudo -u aluno gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'chromium_chromium.desktop', 'firefox_firefox.desktop', 'code_code.desktop', 'sublime-text_subl.desktop', 'dbeaver-ce.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Calculator.desktop']"
