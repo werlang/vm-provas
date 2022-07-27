76782830a44ede5cc1329b8bfc6992c17791924c
Professor <ch-ctinf@ifsul.edu.br>
2022-07-27 17:34:42 -0300

    Change script.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # all VMs.
 # -------------------------------------------------------------
 
-sudo cp -f php.ini /etc/php/8.1/apache2
+sudo cp -f /home/professor/vm-config/php.ini /etc/php/8.1/apache2
 sudo chown root:root /etc/php/8.1/apache2/php.ini
 sudo chmod 644 /etc/php/8.1/apache2/php.ini
 sudo service apache2 restart
