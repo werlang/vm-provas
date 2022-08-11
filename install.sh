8039f9d3e435ef4540cfb25c902a6678adfd67b0
Pablo Werlang <pswerlang@gmail.com>
2022-08-11 01:06:35 -0300

    Add parlot to whitelist.

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # -------------------------------------------------------------
 # This script is used when you want to install something on
-# all VMs.
+# all VMs. It execute on restart.
 # -------------------------------------------------------------
 
-sudo cp -f /home/professor/vm-config/php.ini /etc/php/8.1/apache2
-sudo chown root:root /etc/php/8.1/apache2/php.ini
-sudo chmod 644 /etc/php/8.1/apache2/php.ini
-sudo service apache2 restart
+# sudo cp -f /home/professor/vm-config/php.ini /etc/php/8.1/apache2
+# sudo chown root:root /etc/php/8.1/apache2/php.ini
+# sudo chmod 644 /etc/php/8.1/apache2/php.ini
+# sudo service apache2 restart
