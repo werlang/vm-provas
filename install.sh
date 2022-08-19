90fa76d229b1bcdb1594f2c3a7e6b7284098528a
Professor <ch-ctinf@ifsul.edu.br>
2022-08-19 19:31:53 -0300

    add parlot config on install script.

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@
 # sudo chown root:root /etc/php/8.1/apache2/php.ini
 # sudo chmod 644 /etc/php/8.1/apache2/php.ini
 # sudo service apache2 restart
+
+sudo /home/professor/vm-config/parlot/worker -r lab2 -a
+sudo mv config.json /home/professor/vm-config/parlot/config.json
