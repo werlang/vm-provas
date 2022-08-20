6f381ba4a21ae7af7968a3d37dee332e2dd3f611
Professor <ch-ctinf@ifsul.edu.br>
2022-08-20 15:31:46 -0300

    export path to parlot run script.

--- parlot/run.sh
+++ parlot/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+EXPORT PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin
+
 ROOM="temp-room"
 PATH="/home/professor/vm-config/parlot"
 FILE="config.json"
