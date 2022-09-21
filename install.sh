3e5b445b0433eb68102bdada67267c16618e8c0a
Pablo Werlang <pswerlang@gmail.com>
2022-09-21 17:24:26 -0300

    Add github.dev to whitelist.

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@
 # sudo /home/professor/vm-config/parlot/worker -r lab2 -a
 # sudo mv config.json /home/professor/vm-config/parlot/config.json
 
-sudo /usr/bin/apt install -y default-jre
-sudo /usr/bin/apt install -y snapd
-sudo /usr/bin/snap install --classic eclipse
+# sudo /usr/bin/apt install -y default-jre
+# sudo /usr/bin/apt install -y snapd
+# sudo /usr/bin/snap install --classic eclipse
